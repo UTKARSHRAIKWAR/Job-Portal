372f6b2cf1fcf09aa3b97f856004aeb8bcaa8328
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-02-05 14:42:51 +0530

    finished

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,18 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+// });
